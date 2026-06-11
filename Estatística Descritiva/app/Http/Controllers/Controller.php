4c01e3638bd7f4a9d28b5c5237885b4f6380b919
leandroDantas <leandrodantasdev@gmail.com>
2026-06-11 11:38:23 -0300

    feat: ✨ add relationships for Order, OrderItem, Product, Review, and User models

--- Estatística Descritiva/app/Http/Controllers/Controller.php
+++ Estatística Descritiva/app/Http/Controllers/Controller.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\View\View;
 
 abstract class Controller
 {
     public function slow(): View
     {
-        return view('slow-query');
+        return view('slow-query', [
+            'products' => Product::all(),
+        ]);
     }
 
     public function fast(): View
--- Estatística Descritiva/app/Models/Product.php
+++ Estatística Descritiva/app/Models/Product.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
-    //
+    public function reviews(): HasMany
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function favorites(): HasMany
+    {
+        return $this->hasMany(Favorite::class);
+    }
 }
